d741d5bc3eaf7b1e720027f77b1b5b36bb4a394d
Felipe Gasper <felipe@felipegasper.com>
2020-02-06 07:23:28 -0500

    0.07 release

--- lib/Net/Curl/Promiser.pm
+++ lib/Net/Curl/Promiser.pm
@@ -3,7 +3,7 @@ package Net::Curl::Promiser;
 use strict;
 use warnings;
 
-our $VERSION = '0.07_08';
+our $VERSION = '0.07';
 
 =encoding utf-8
 
--- lib/Net/Curl/Promiser/LoopBase.pm
+++ lib/Net/Curl/Promiser/LoopBase.pm
@@ -38,6 +38,10 @@ use parent qw( Net::Curl::Promiser );
 
 use Net::Curl ();
 
+# In production there’s probably little reason to care about this,
+# but it might be useful for debugging.
+use constant _STRICT_STOP_POLL => 0;
+
 #----------------------------------------------------------------------
 
 *_process_in_loop = __PACKAGE__->can('SUPER::process');
@@ -45,7 +49,7 @@ use Net::Curl ();
 
 # 7.66 is observed not to have this problem;
 # assumedly newer libcurls won’t regress.
-use constant _MINIMUM_LIBCURL_TO_WARN_ABOUT_EXTRA_STOP_POLL => 7.66;
+use constant _MINIMUM_LIBCURL_TO_WARN_ABOUT_EXTRA_STOP_POLL => 7.52;
 
 sub process { die 'Unneeded method: ' . (caller 0)[3] };
 sub get_timeout { die 'Unneeded method: ' . (caller 0)[3] };
@@ -58,16 +62,18 @@ sub _GET_FD_ACTION {
 sub _handle_extra_stop_poll {
     my ($self, $fd) = @_;
 
-    my $version = Net::Curl::LIBCURL_VERSION();
+    if (_STRICT_STOP_POLL) {
+        my $version = Net::Curl::LIBCURL_VERSION();
 
-    if ( $version =~ m<\A([0-9]+\.[0-9]+)> ) {
-        if ($1 >= _MINIMUM_LIBCURL_TO_WARN_ABOUT_EXTRA_STOP_POLL) {
-            my $ref = ref $self;
-            warn "$ref: Unexpected “extra” FD stop (libcurl $version): [$fd]";
+        if ( $version =~ m<\A([0-9]+\.[0-9]+)> ) {
+            if ($1 >= _MINIMUM_LIBCURL_TO_WARN_ABOUT_EXTRA_STOP_POLL) {
+                my $ref = ref $self;
+                warn "$ref: Unexpected “extra” FD stop (libcurl $version): [$fd]";
+            }
         }
-    }
-    else {
-        warn "Unparseable LIBCURL_VERSION: [$version]";
+        else {
+            warn "Unparseable LIBCURL_VERSION: [$version]";
+        }
     }
 
     return;
